7f58a37ff004fa58564ee8ff74f850c9135966f7
Himanshu Mahajan <himanshumahajan456@gmail.com>
2022-05-12 18:28:07 -0400

    Azure Function for counting visitor

--- frontend/main.js
+++ frontend/main.js
@@ -2,12 +2,13 @@ window.addEventListener("DOMContentLoaded", (event) => {
     getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/CounterHttpTrigger';
+const functionApiUrl = 'https://azureresumecounterfunc.azurewebsites.net/api/CounterHttpTrigger?code=0g4AfqGvwWLChyy4Qnai8kb8Z9KDvJ9bS8L_VnUYZh0ZAzFu1JOGnQ=='
+const localFunctionApi = 'http://localhost:7071/api/CounterHttpTrigger';
 
 const getVisitCount = () => {
     let count = 0;
 
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response => {
         const count = response;
